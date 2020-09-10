2e43733e2e652a0e2ff27e4bf28635f4d5b4f804
phakawatchu <67142373+phakawatchu@users.noreply.github.com>
2020-09-10 15:13:27 +0700

    Update matrix_include_1strow_1stcolumn.py

--- matrix_include_1strow_1stcolumn.py
+++ matrix_include_1strow_1stcolumn.py
@@ -1,15 +1,15 @@
 import numpy as np
 #put the input file name in the single quote
-matrix = np.loadtxt('input.txt') 
+matrix = np.loadtxt('dm2.test')
 print (matrix)
 
 rows = len(matrix) #determine the row size
 columns = len(matrix[0]) #determine the column size
 
-print ("Number of rows is " , rows)
-print ("Number of columns is " , columns)
+#print ("Number of rows is " , rows)
+#print ("Number of columns is " , columns)
 
-for i in range(rows):
- for j in range(columns):
-  if matrix[i,j] <= 1.0: #define the cutoff here
-   print ("Row is ",i," Column is ",j," value is ",matrix[i,j])
+for i in range(0,319):#first block range
+ for j in range(320,390): #second block range
+  if matrix[i,j] < 0.6: #define the cutoff here
+   print (i+1," ",j+1," ",matrix[i,j])
